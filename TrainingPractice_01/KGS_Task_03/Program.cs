524b8ef44f4332c959b17093076b5ffde621d486
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-02 17:48:37 +0300

    Improve third task

--- TrainingPractice_01/KGS_Task_03/Program.cs
+++ TrainingPractice_01/KGS_Task_03/Program.cs
@@ -8,24 +8,37 @@ namespace KGS_Task_3
         {
             const string password = "a-VERY-strong-password";
             string input = "";
+            int attemptCounter;
 
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Введите пароль: ");
             Console.ForegroundColor = ConsoleColor.White;
 
-            while (input != password)
+            for (attemptCounter = 1; (input != password) && (attemptCounter <= 3); attemptCounter++)
             {
                 if (input != "")
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("Введен неверный пароль. Введите пароль: ");
+                    Console.Write($"Введен неверный пароль. У вас осталось {3 - attemptCounter} попыток. Введите пароль: ");
                     Console.ForegroundColor = ConsoleColor.White;
                 }
 
                 input = Console.ReadLine();
             }
 
-            Console.WriteLine("\nСекретное сообщение!");
+            if (input == password)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nСекретное сообщение!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n3 неверные попытки, закрытие программы.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
         }
     }
 }
