45b746b5fee4283892935aa5b470f24d02830030
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-02 16:22:43 +0300

    Add third task

---
+++ KGS_Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KGS_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "a-VERY-strong-password";
+            string input = "";
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Введите пароль: ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (input != password)
+            {
+                if (input != "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Введен неверный пароль. Введите пароль: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nСекретное сообщение!");
+        }
+    }
+}
