ee2033618e3d7fbe19dacaec904e00e4949ea6c2
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-02 11:58:33 +0300

    Add second task

---
+++ KGS_Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KGS_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+                Console.WriteLine($"Вы ввели {input}.");
+            }
+
+            Console.WriteLine("\nВы вышли из цикла!");
+        }
+    }
+}
