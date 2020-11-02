a82af4aa507598300f54672973176a9a5332f1b5
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-02 11:21:25 +0300

    Implement fool proofing

--- KGS_Task_1/Program.cs
+++ KGS_Task_1/Program.cs
@@ -6,13 +6,45 @@ namespace KGS_Task_1
     {
         static void Main(string[] args)
         {
-            const int price = 3; // Цена 1 кристалла
+            const int price = 2; // Цена 1 кристалла
+            int gold = 0;
+            int crystals = 0;
 
-            Console.Write("Количество золота: ");   
-            int gold = Int32.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Количество золота: ");   
+                bool successful_conversion = Int32.TryParse(Console.ReadLine(), out gold);
+
+                if (!successful_conversion)
+                    throw new ArithmeticException("Количество денег должно быть числом.");
+
+                if (gold <= 0)
+                    throw new ArithmeticException("Введено неверное количество денег.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); // Вывод ошибки
+                Environment.Exit(1); // Закончить программу из-за ошибки
+            }
+            
             Console.WriteLine($"{price} - цена за 1 кристалл");
-            Console.Write("Сколько кристаллов вы хотите купить: ");
-            int crystals = Int32.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.Write("Сколько кристаллов вы хотите купить: ");
+                bool successful_conversion = Int32.TryParse(Console.ReadLine(), out crystals);
+
+                if (!successful_conversion)
+                    throw new ArithmeticException("Количество денег должно быть числом.");
+
+                if (crystals <= 0)
+                    throw new ArithmeticException("Введено неверное количество кристаллов.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
 
             int can_buy = gold / price; // Сколько кристаллов можно купить за все деньги
 
