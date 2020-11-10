70efbc399667f4d75388a18ea7444d2e9ac1102c
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-10 14:03:57 +0300

    Fix numbers not showing

--- TrainingPractice_02/Program.cs
+++ TrainingPractice_02/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 
-// https://gist.github.com/gkeep/4db57a585b5ddbed73a1f99da71a837e#file-1-cpp
+/*
+ * SoE implementation on C++: https://gist.github.com/gkeep/4db57a585b5ddbed73a1f99da71a837e#file-1-cpp
+ * 
+ * Prime numbers up to 100:
+ * 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+ */
 
 namespace TrainingPractice_02
 {
@@ -38,15 +43,20 @@ namespace TrainingPractice_02
             Console.SetCursorPosition(pos_x, pos_y);
         }
 
-        static void printArray(bool[] array, int highlight_index)
+        static void printArray(bool[] array, int highlightIndex, ConsoleColor highlightColor)
         {
             Console.Clear();
             printPrimeNumbers(Console.CursorLeft, Console.CursorTop);
 
             for (int i = 0; i < array.Length; i++)
             {
-                if (i == highlight_index)
-                    Console.ForegroundColor = ConsoleColor.Red;
+                if (i < highlightIndex)
+                    fillPrimeArray(array, highlightIndex);
+
+                if (i == highlightIndex)
+                    Console.ForegroundColor = highlightColor;
+                else if (array[i] && i < highlightIndex)
+                    Console.ForegroundColor = ConsoleColor.Green;
 
                 Console.Write($"{Convert.ToInt32(array[i])} ");
 
@@ -62,28 +72,24 @@ namespace TrainingPractice_02
         static void printArrayInProgress(bool[] array)
         {
             for (int i = 2; i * i < array.Length; i++)
-            {
                 if (array[i])
-                {
                     for (int j = i * i; j < array.Length; j += i)
                     {
                         array[j] = false;
-                        printArray(array, j);
+                        printArray(array, j, ConsoleColor.Red);
                     }
-                }
-            }
         }
 
-        static void fillPrimeArray(bool[] array)
+        static void fillPrimeArray(bool[] array, int stop_index)
         {
+            primeArray = new int[primeArray.Length]; // обнуляем массив, чтобы избавиться от лишних чисел
             int iter = 0;
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 1; i < stop_index; i++)
                 if (array[i])
                 {
                     primeArray[iter] = i;
                     iter++;
                 }
-            //printPrimeNumbers(Console.CursorLeft, Console.CursorTop);
         }
 
         static void Main(string[] args)
@@ -106,7 +112,6 @@ namespace TrainingPractice_02
             bool[] arrayOfOnes = InitialArray(size);
             primeArray = new int[size];
 
-            fillPrimeArray(arrayOfOnes);
             printArrayInProgress(arrayOfOnes);
         }
     }
